95cc7226aa9bbdfffee2ef3c16271dd36798bf2b
Javier Celaya <javier.celaya@flexvdi.com>
2019-06-13 09:26:54 +0200

    Update cerbero submodule if needed

--- cerbero.sh
+++ cerbero.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-cd $(dirname "$(readlink -f $0)")/cerbero/config
+SRC_DIR="$(dirname "$(readlink -f $0)")"
+CONFIG_DIR="$SRC_DIR"/cerbero/config
+
+[ -d "$CONFIG_DIR" ] || {
+    cd "$SRC_DIR"
+    git submodule init && git submodule update
+} || {
+    echo Downloading the Cerbero submodule failed
+    exit 1
+}
+
+cd "$CONFIG_DIR"
 ../cerbero-uninstalled -c ../../flexvdi.cbc "$@"
 
